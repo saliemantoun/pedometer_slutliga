d213089c5f2acc736f96d9b7607e4113b83817a8
saliemantoun <saliemantoon@gmail.com>
2022-02-08 14:49:26 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pedometer_slutliga/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pedometer_slutliga
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,3 @@
+
+//TODO: Här ska ni radera allt som finns och klistra in hela exempel som finns i länken: (https://github.com/cph-cachet/flutter-plugins/blob/master/packages/pedometer/example/lib/main.dart)
+
